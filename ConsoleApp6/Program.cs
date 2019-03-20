7200bafa2f03ce1772e9f47cdbf0a552deddde63
Moksiy <MaksAligin@yandex.ru>
2019-03-20 09:13:43 +0300

    Работающее меню удаления улицы и автостоянки по индексу

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1961,6 +1961,9 @@ namespace ConsoleApp6
                             break;
 
                         case 2:
+                            //Очистка консоли
+                            Console.Clear();
+
 
                             break;
 
@@ -1983,7 +1986,162 @@ namespace ConsoleApp6
             //Очистка консоли
             Console.Clear();
 
+            //Создание переменной для хранения номера выбранного пункта меню
+            int j = 1;
 
+            //Создание экземпляра класса с методами построения меню
+            Consolebd consolebd = new Consolebd();
+
+            //Создание экземпляра класса с данными
+            Data data = new Data();
+
+            //Объявление переменной для хранения информации о нажатой клавише
+            ConsoleKeyInfo key;
+
+            //основной блок выполнения
+            do
+            {
+                //очистка консоли
+                Console.Clear();
+
+                //Вызов метода построения меню удаления
+                consolebd.ChoiseTypeOfDelete(j);
+
+                //Считывание инормации о нажатой клавише
+                key = Console.ReadKey(true);
+
+                //Условие для отсеивания ложных нажатий
+                if (key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.Enter)
+                {
+                    //Очистка консоли
+                    Console.Clear();
+
+                    //оператор присвоения пункта меню
+                    switch (key.Key)
+                    {
+                        //UPARROW
+                        case ConsoleKey.UpArrow:
+                            j--;
+                            if (j < 1) { j = 4; }
+                            if (j > 4) { j = 1; }
+                            break;
+
+                        //DOWNARROW
+                        case ConsoleKey.DownArrow:
+                            j++;
+                            if (j < 1) { j = 4; }
+                            if (j > 4) { j = 1; }
+                            break;
+                    }
+
+                }
+
+                //Вызов метода построения меню выбора типа удаления элемента
+                consolebd.ChoiseTypeOfDelete(j);
+
+                //Выбор пункта меню удаления элемента
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    //Выбор пункта меню
+                    switch(j)
+                    {
+                        //Удаление элемента по индексу
+                        case 1:
+                            //Очистка консоли
+                            Console.Clear();
+
+                            //Вызов метода построения меню удалени элемента по индексу
+                            consolebd.DeleteElementIndex();
+
+                            //Перемещение каретки в нужную позицию
+                            Console.SetCursorPosition(27, 3);
+
+                            //Объявление переменной для хранения введенного индекса
+                            int index = 0;
+
+                            //Считывание введенного индекса
+                            string ind = Console.ReadLine();
+
+                            //проверка на возможность конвертации в int
+                            if (int.TryParse(ind, out index))
+                            {
+                                //Присвоение
+                                index = int.Parse(ind);
+                            }
+                            else
+                            {
+                                //Очистка консоли
+                                Console.Clear();
+
+                                //Вывод предупреждения
+                                consolebd.Attention();
+
+                                //очистка консоли
+                                Console.Clear();
+
+                                //Обнуление переменной после некорректного присвоения
+                                ind = " ";
+
+                                //Вызов метода повторного построения меню удаления
+                                consolebd.DeleteElementIndex();
+
+                                //Повторный вызов текущего метода
+                                DeleteParking();
+                            }
+                            //Декремент для корректной работы
+                            index--;
+
+                            //переменная для хранения количества элементов в списке таблицы 3
+                            int count = data.CountTab3();
+
+                            //проверка на существование элемента повведенному индексу
+                            if (index < count)
+                            {
+                                //Удаление элемента списка после успешной проверки
+                                data.DeleteElementTab2(index);
+
+                                //очистка консоли
+                                Console.Clear();
+
+                                //Выход в меню выбора удаления элемента
+                                consolebd.DeleteMenu(j);
+
+                                //Вызов метода считывания нажатых клавиш
+                                DeleteMenu();
+                            }
+                            else
+                            {
+                                //очистка консоли
+                                Console.Clear();
+
+                                //Вызов метода построения окна предупреждения
+                                consolebd.AttentionDeleteElement();
+
+                                //очистка консоли
+                                Console.Clear();
+
+                                //Вызов меню удаления элемента
+                                consolebd.DeleteMenu(j);
+
+                                //Вызов меню считывания клавиш
+                                DeleteMenu();
+
+                                
+                            }
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+                    }
+                }
+
+            } while (key.Key != ConsoleKey.Enter);
         }
 
         //Метод меню удаления акта эвакуации
